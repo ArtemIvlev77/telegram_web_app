b192679f69eb441c582885928beaf657a2d42085
Artem Ivlev <artemivlev@yandex.ru>
2023-03-16 20:01:03 +0300

    check

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'bg-secondary': 'var(--tg-theme-secondary-bg-color)',
+        'secondary': 'var(--tg-theme-secondary-bg-color)',
         
       },
     },

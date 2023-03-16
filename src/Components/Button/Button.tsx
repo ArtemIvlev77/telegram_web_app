e6a3ac0053b8de33d2e77d2228082643da5314ba
Artem Ivlev <artemivlev@yandex.ru>
2023-03-16 15:35:17 +0300

    check

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps extends PropsWithChildren{
 	className?: string;
 	onClick: () => void;
 }
-const Button:FC<ButtonProps> = (props, children) => {
+const Button:FC<ButtonProps> = (props, {children}) => {
 	return (
 		<button {...props} className={'button ' + props.className}>
 			{children}

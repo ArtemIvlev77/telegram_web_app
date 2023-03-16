28416a9771906aa46da33e32de6469039e0a65fb
Artem Ivlev <artemivlev@yandex.ru>
2023-03-16 23:34:43 +0300

    check

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,22 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '../Button/Button';
 import {useTelegram} from '../../hooks/useTelegram';
 import './Header.css'
 
 const Header = () => {
+	const races = [
+		{invoiceNumber: 233, invoiceTitle: 'Ташкент-махачкала'},
+		{invoiceNumber: 211, invoiceTitle: 'Ташкент-махачкала'},
+		{invoiceNumber: 21133, invoiceTitle: 'Ташкент-махачкала'},
+		{invoiceNumber: 2433, invoiceTitle: 'Ташкент-махачкала'},
+		{invoiceNumber: 2533, invoiceTitle: 'Ташкент-махачкала'},
+		{invoiceNumber: 733, invoiceTitle: 'Ташкент-махачкала'},
+	]
+
+	const organizations = [
+		{invoiceNumber: 223, organization: 'OOO Ромашка'},
+		{invoiceNumber: 255, organization: 'OOO OGon'},
+		{invoiceNumber: 223, organization: 'OOO TOP'},
+		{invoiceNumber: 6556, organization: 'OOO SAMLOG'},
+		{invoiceNumber: 544, organization: 'OOO Ромашка'},
+	]
+
 	const {onClose, user, tg } = useTelegram()
+	const [role, setRole] = useState('Грузоперевозчик')
+	const [race, setRace] = useState();
+	const [organization, setOrganization] = useState();
 	const roleChangeHandler = () => {
-		console.log('roleChanged')
+		if (role === 'Грузоперевозчик') {
+			setRole('Заказчик')
+		} else {
+			setRole( 'Грузоперевозчик')
+		}
 	}
+
+	const raceHandler  = (e: any) => {
+		setRace(e.target.value)
+	}
+
+	const organizationChangeHandler = (e:any) => {
+		setOrganization(e.target.value)
+	}
+
 	return (
 		<header className='bg-secondary h-1/4 flex px-5 justify-between items-center'>
 			<div className="flex flex-col gap-1 justify-center">
 				<span>{user?.username ?? 'Username'}</span>
 				<span>ID сервиса</span>
-				<div className="flex items-center">
-				<span className="text-center">Грузоперевозчик	</span>
+				<div className="flex items-center justify-between gap-2">
+				<span className="text-center">{role}</span>
 					<Button className="bg-[url('/src/assets/refresh.svg')] bg-no-repeat bg-center" onClick={roleChangeHandler}></Button>
 				</div>
+			</div>
+			<div className="flex flex-col gap-1">
+				Мои организации
+				<select name="organizations" id="organizations" value={organization} onChange={(e) => organizationChangeHandler(e)}>
+					{organizations.map((org) => (
+						<option value={org.invoiceNumber}>{org.organization}</option>
+					))}
+				</select>
+				Мои рейсы
+				<select name="races" id="races" value={race} onChange={(e) => raceHandler(e)}>
+					{races.map((race) => (
+						<option value={race.invoiceNumber}>#{race.invoiceNumber} {race.invoiceTitle}</option>
+					))
+					}
+
+				</select>
 			</div>
 		</header>
 	);

1a7f5ef79b0755ddfecbc00d368520bb016a25d7
Artem Ivlev <artemivlev@yandex.ru>
2023-03-16 17:48:33 +0300

    check

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Button from '../Button/Button';
 import {useTelegram} from '../../hooks/useTelegram';
 import './Header.css'
+import person from '../../assets/person.svg'
 
 const Header = () => {
 	const {onClose, user } = useTelegram()
 	return (
 		<header className='header'>
 			<div className="header__userInfo">
-				<img src={user?.photo_url} alt="avatar"/>
+				<img src={user?.photo_url ?? person} alt="avatar"/>
 				<span className={'username'}>{user?.username ?? 'Username'}</span>
 			</div>
 			<Button onClick={onClose}>Закрыть</Button>

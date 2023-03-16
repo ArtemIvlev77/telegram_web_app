f0723bad1f2bb51dc28adcc9e88a5212445cf1ea
Artem Ivlev <artemivlev@yandex.ru>
2023-03-16 19:49:31 +0300

    check

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -10,7 +10,7 @@ const Table = () => {
 		{invoiceNumber: '544', organization: 'OOO Ромашка', status: 'closed', messages: 15, action: '', delete: ''},
 	]
 	return (
-		<div className="relative overflow-x-auto">
+		<div className="relative mt-5">
 			<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
 				<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 				<tr>

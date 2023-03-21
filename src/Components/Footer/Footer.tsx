07c7f613cb88b53a1f0a087aa2b12a1c76f5e637
Artem Ivlev <artemivlev@yandex.ru>
2023-03-21 15:54:27 +0300

    check

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,9 @@
 
 const Footer = () => {
 	return (
-		<div>
-			<button >
-
+		<div className='fixed bottom-4 bg-tg-button left-0 w-full h-6'>
+			<button className='p-2 rounded-2xl w-full text-tg-text-button'>
+				Создать заявку
 			</button>
 		</div>
 	);

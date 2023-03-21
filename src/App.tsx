9f48aef43b83895ca67d570cece3be636a8647c7
Artem Ivlev <artemivlev@yandex.ru>
2023-03-21 17:21:42 +0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect} from 'react';
 import './App.css';
-import Header from './Components/Header/Header';
+import Header from './components/Header/Header';
 import {useTelegram} from './hooks/useTelegram';
-import Chart from './Components/Chart/Chart';
+import Chart from './components/Chart/Chart';
 import {Route, Routes} from 'react-router-dom';
-import Footer from './Components/Footer/Footer';
-import Orders from './Components/Orders/Orders';
-import Races from './Components/Races/Races';
+import Footer from './components/Footer/Footer';
+import Orders from './components/Orders/Orders';
+import Races from './components/Races/Races';
 
 function App() {
 	const {tg, user, onToggleButton} = useTelegram()
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
 	return (
-		<footer className='fixed bottom-2 bg-tg-secondary-bg left-0 w-full h-[10%] p-2'>
+		<footer className='fixed bottom-0 bg-tg-secondary-bg left-0 w-full h-[10%] p-2'>
 			<button className='p-4 rounded-2xl bg-tg-button w-full text-tg-text-button'>
 				Создать Рейс
 			</button>

bf5c16ba097697112932b940498dbadbf5857cf3
Artem Ivlev <artemivlev@yandex.ru>
2023-03-21 15:59:24 +0300

    check

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
 	return (
 		<div className='fixed bottom-12 bg-tg-secondary-bg left-0 w-full h-6 p-4'>
-			<button className='p-8 rounded-2xl bg-tg-button w-full text-tg-text-button'>
+			<button className='p-4 rounded-2xl bg-tg-button w-full text-tg-text-button'>
 				Создать Рейс
 			</button>
 		</div>

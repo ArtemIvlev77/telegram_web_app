b4cf29dc54ecd3c42263ecf2c2dfcabd4a666352
Artem Ivlev <artemivlev@yandex.ru>
2023-03-24 11:44:32 +0300

    check

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,19 +2,21 @@ import axios, {AxiosError} from 'axios';
 import {BASE_URL} from '../utils/constats';
 import {UserInfoType} from '../utils/types';
 
-export const getUserData = async (userId: number) => {
-	try {
-
+export const getUserData = (userId: number) => {
+		async function fetchData(): Promise<UserInfoType> {
+			try {
 		const response = await axios.get(`${BASE_URL}/GetUserData?tgid=${userId}`)
 		if (response.data) {
 			return response.data
-		}
-		return null
-	} catch (err: unknown | AxiosError) {
-		if (axios.isAxiosError(err)) {
-			console.log(err)
 		} else {
-			console.log(err)
-		}
+			throw new Error('Ошибка при получении данных пользователя')}
+		} catch (err: unknown | AxiosError) {
+			if (axios.isAxiosError(err)) {
+				console.log(err)
+			} else {
+				console.log(err)
+			}
+		return fetchData()
+
 	}
 }
